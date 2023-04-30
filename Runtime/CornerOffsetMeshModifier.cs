0e108189ba3b6420666dffcc7a3288437219eb08
gilzoide <gilzoide@gmail.com>
2023-04-30 08:18:40 -0300

    Add support for normalized offset

--- Runtime/CornerOffsetMeshModifier.cs
+++ Runtime/CornerOffsetMeshModifier.cs
@@ -7,13 +7,16 @@ namespace Gilzoide.GraphicCornerOffset
     public class CornerOffsetMeshModifier : BaseMeshEffect
     {
         [Tooltip("Offset applied to the bottom left corner")]
-        public Vector3 BottomLeft;
+        public Vector2 BottomLeft;
         [Tooltip("Offset applied to the top left corner")]
-        public Vector3 TopLeft;
+        public Vector2 TopLeft;
         [Tooltip("Offset applied to the top right corner")]
-        public Vector3 TopRight;
+        public Vector2 TopRight;
         [Tooltip("Offset applied to the bottom right corner")]
-        public Vector3 BottomRight;
+        public Vector2 BottomRight;
+        [Tooltip("If false, offsets use absolute position in units."
+            + "\n\nIf true, offsets use normalized position from 0 to 1, where 1 means 100% of the width in X or 100% of the height in Y.")]
+        public bool NormalizedOffset;
 
         public override void ModifyMesh(VertexHelper vh)
         {
@@ -23,17 +26,28 @@ namespace Gilzoide.GraphicCornerOffset
             for (int i = 0; i < vh.currentVertCount; i++)
             {
                 vh.PopulateUIVertex(ref vertex, i);
-
-                Vector2 normalizedPosition = Rect.PointToNormalized(rect, vertex.position);
-
-                vertex.position = vertex.position
-                    + BottomLeft * (1f - normalizedPosition.x) * (1f - normalizedPosition.y)
-                    + TopLeft * (1f - normalizedPosition.x) * normalizedPosition.y
-                    + TopRight * normalizedPosition.x * normalizedPosition.y
-                    + BottomRight * normalizedPosition.x * (1f - normalizedPosition.y);
-
+                ApplyModifier(ref vertex.position, rect);
                 vh.SetUIVertex(vertex, i);
             }
+        }
+
+        private void ApplyModifier(ref Vector3 position, Rect rect)
+        {
+            Vector3 offset = (Vector3) GetModifierValue(position, rect);
+            if (NormalizedOffset)
+            {
+                offset *= rect.size;
+            }
+            position += offset;
+        }
+
+        private Vector2 GetModifierValue(Vector2 position, Rect rect)
+        {
+            Vector2 normalizedPosition = Rect.PointToNormalized(rect, position);
+            return BottomLeft * (1f - normalizedPosition.x) * (1f - normalizedPosition.y)
+                + TopLeft * (1f - normalizedPosition.x) * normalizedPosition.y
+                + TopRight * normalizedPosition.x * normalizedPosition.y
+                + BottomRight * normalizedPosition.x * (1f - normalizedPosition.y);
         }
     }
 }
